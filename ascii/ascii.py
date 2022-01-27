03e5b280190c020a43e97d66b93bbcda0a8dcd4e
itisdb <dhriteshbhagat@gmail.com>
2022-01-27 23:58:12 +0530

    Update structure

--- ascii/ascii.py
+++ ascii/ascii.py
@@ -54,7 +54,7 @@ def ascii_art(num):
         text_file.write('\n')
 
     outputImage = ImageEnhance.Brightness(outputImage).enhance(2)
-    outputImage = ImageEnhance.Contrast(outputImage).enhance(0.69)
+    outputImage = ImageEnhance.Contrast(outputImage).enhance(1)
     outputImage = ImageEnhance.Color(outputImage).enhance(5)
     outputImage = ImageEnhance.Sharpness(outputImage).enhance(0.5)
     outputImage.save('./outputs/nft'+str(num)+'.jpg')
